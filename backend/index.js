f431d888868ddef3a99d0eb01e757f5593110321
lij110397 <lij110397@163.com>
2024-04-21 21:44:09 +0800

    static frontend folder and changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,26 @@
 const express = require('express')
 const morgan = require('morgan')
-const Person = require('./models/person')
-require('dotenv').config()
+// 设置CORS
+const cors = require('cors')
+
 const app = express()
 app.use(express.json())
-//app.use(morgan('tiny'))
+app.use(cors())
+const Person = require('./models/person')
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+require('dotenv').config()
+
+// app.use(morgan('tiny'))
 
 // 自定义 Morgan 日志输出格式，包含请求体数据
 morgan.token('req-body', (req) => JSON.stringify(req.body))
 // 配置 Morgan，使用自定义格式化器
 app.use(
   morgan(
-    ':method :url :status :res[content-length] - :response-time ms :req-body'
-  )
+    ':method :url :status :res[content-length] - :response-time ms :req-body',
+  ),
 )
-
-//设置CORS
-const cors = require('cors')
-app.use(cors())
 
 // let persons = [
 //     {
@@ -48,11 +51,11 @@ app.get('/api/persons', (request, response, next) => {
       response.json(people)
     })
     .catch((error) => next(error))
-  //response.json(persons)
+  // response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  //const id = Number(request.params.id)
+  // const id = Number(request.params.id)
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -73,7 +76,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.delete('/api/persons/:id', (request, response, next) => {
   //   const id = Number(request.params.id)
   //   console.log('delete id', id)
-  //persons = persons.filter(person => person.id === id)
+  // persons = persons.filter(person => person.id === id)
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end()
@@ -84,7 +87,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { body } = request
 
   const person = {
     name: body.name,
@@ -99,9 +102,9 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body
-  //console.log('body name ', body.name)
-  //console.log('body number ', body.number)
+  const { body } = request
+  // console.log('body name ', body.name)
+  // console.log('body number ', body.number)
   // let existName = persons.find(person => person.name === body.name) ? true:false
   // let existNumber= persons.find(person => person.number === body.number) ? true:false
 
@@ -130,28 +133,28 @@ app.post('/api/persons', (request, response, next) => {
     number: body.number,
   })
 
-  //persons = persons.concat(person)
+  // persons = persons.concat(person)
   person
     .save()
     .then((savePerson) => {
       response.json(savePerson)
     })
     .catch((error) => next(error))
-  //response.json(person)
+  // response.json(person)
 })
 
 // generate an id which does not exist
-const generateId = () => {
-  let random
-  let exist = true
-  while (exist) {
-    random = Math.floor(Math.random() * 100) + 1
-    exist = persons.find((person) => person.id === random) ? true : false
-    //console.log(exist)
-  }
+// const generateId = () => {
+//   let random
+//   let exist = true
+//   while (exist) {
+//     random = Math.floor(Math.random() * 100) + 1
+//     exist = persons.find((person) => person.id === random) ? true : false
+//     //console.log(exist)
+//   }
 
-  return random
-}
+//   return random
+// }
 
 app.get('/info', (request, response, next) => {
   let number = 0
@@ -161,7 +164,7 @@ app.get('/info', (request, response, next) => {
         number = people.length
         const timeNow = new Date()
         response.send(
-          `<p>Phonebook has info for ${number} people</p><p>${timeNow}</p>`
+          `<p>Phonebook has info for ${number} people</p><p>${timeNow}</p>`,
         )
       } else {
         throw new Error('No data found')
@@ -170,7 +173,7 @@ app.get('/info', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-//错误处理中间件
+// 错误处理中间件
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -179,17 +182,16 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-  
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
+
+  next(error)
+}
 // this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler)
 
